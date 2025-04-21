2230e92757c438d5645554ff839c7f3d0474bebf
infosys <infosys@C4220>
2025-04-21 09:57:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter01/ProductSample/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ProductSample {
+
+    internal class Program {
+
+        static void Main(string[] args)
+        {
+            Product karinto = new Product(123, "かりんとう", 178); 
+            Product daifuku = new Product(124, "大福", 138);
+            Product dorayaki = new Product(125, "どら焼き", 238);
+
+            //税抜き価格を表示
+            Console.WriteLine(karinto.Name + "の税抜き価格は" + karinto.Price + "円です");
+            //消費税額の表示
+            Console.WriteLine(karinto.Name + "の消費税額は" + karinto.GetTax() + "円です");
+            //税込み価格の表示
+            Console.WriteLine(karinto.Name + "の税込み価格は" + karinto.GetPriceIncludingTax() + "円です");
+
+            //税抜き価格を表示
+            Console.WriteLine(daifuku.Name + "の税抜き価格は" + daifuku.Price + "円です");
+            //消費税額の表示
+            Console.WriteLine(daifuku.Name + "の消費税額は" + daifuku.GetTax() + "円です");
+            //税込み価格の表示
+            Console.WriteLine(daifuku.Name + "の税込み価格は" + daifuku.GetPriceIncludingTax() + "円です");
+
+            //税抜き価格を表示
+            Console.WriteLine(dorayaki.Name + "の税抜き価格は" + dorayaki.Price + "円です");
+            //消費税額の表示
+            Console.WriteLine(dorayaki.Name + "の消費税額は" + dorayaki.GetTax() + "円です");
+            //税込み価格の表示
+            Console.WriteLine(dorayaki.Name + "の税込み価格は" + dorayaki.GetPriceIncludingTax() + "円です");
+        }
+    }
+}
---
+++ Chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+            /* -tom << メートルへ変換
+             * -tof <, フィートへ変換  */
+
+            //if (args.Length >= 1 && args[0] == "-tom" ... のような指定あるとなお良し
+
+            int arg2 = 0;
+            int arg3 = 0;
+
+            if (args.Length == 2) {
+                arg2 = int.Parse(args[1]);
+            } else if (args.Length == 3) {
+                arg2 = int.Parse(args[1]);
+                arg3 = int.Parse(args[2]);
+            } else {
+            }
+
+            if (args.Length == 0) { //args[0]が無い場合
+                Console.WriteLine("例外が発生しました\nargs[0]に何も指定されていません");
+                
+                //-tom -tofの振り分けとargs.Lengthの判定
+            } else if (args.Length == 1 && args[0] == "-tom") {
+                PrintFeetToMeterList(1, 10);
+
+            } else if (args.Length == 1 && args[0] == "-tof") {
+                PrintMeterToFeetList(1, 10);
+
+            } else if (args.Length == 2 && args[0] == "-tom") {
+                PrintFeetToMeterList(arg2, arg2 + 10);
+
+            } else if (args.Length == 2 && args[0] == "-tof") {
+                PrintMeterToFeetList(arg2, arg2 + 10);
+
+            } else if (args.Length == 3 && args[0] == "-tom") {
+                PrintFeetToMeterList(arg2, arg3);
+
+            } else if (args.Length == 3 && args[0] == "-tof") {
+                PrintMeterToFeetList(arg2, arg3);
+
+            } else if (args.Length >= 4) {  //引数が4つ以上指定された場合
+                Console.WriteLine("例外が発生しました\n引数が多すぎます");
+
+            } else {    //非対応の引数が指定された場合
+                Console.WriteLine("例外が発生しました\n無効な引数です");
+            }
+
+            //FeetToMeterの結果をConsoleにPrintする
+            static void PrintFeetToMeterList(int start, int end) {
+                for (int feet = start; feet <= end; feet++) {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+                }
+            }
+
+            //MeterToFeetの結果をConsoleにPrintする
+            static void PrintMeterToFeetList(int start, int end) {
+                for (int meter = start; meter <= end; meter++) {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+                }
+            }
+
+            //フィートからメートルへ変換するメソッド
+            static double FeetToMeter(int Feet) {
+            return Feet * 0.3048;
+            }
+            //メートルからフィートへ変換するメソッド
+            static double MeterToFeet(int Meter) {
+                return Meter / 0.3048;
+            }
+
+        }
+    }
+}
