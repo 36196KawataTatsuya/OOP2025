19d9bc91acfcc4c15c0070b5e6c05d3a894fb668
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-04-22 11:37:13 +0900

    インチからメートル変換完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class InchConverter {
+
+        //定数
+        private const double ratio = 0.0254;
+
+        //インチからメートルへ変換して返す
+        public static double ToMeter(double Inch) {
+            return Inch * ratio;
+        }
+
+        //メートルからインチへ変換して返す
+        public static double FromMeter(double Meter) {
+            return Meter / ratio;
+        }
+
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            }
+
+        }
+    }
+}
