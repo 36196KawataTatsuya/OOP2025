95bfec597fe8dd436680aa813233fbff0ef93a2b
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-09 15:59:27 +0900

    暗黙の型指定を使用 (var)

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -3,10 +3,10 @@
 namespace SalesCalculator {
     internal class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(@"data\sales.csv"); 
+            var sales = new SalesCounter(@"data\sales.csv"); 
             //ここでリスト化せず集計処理機能にパスを渡して内部でリスト化してもらう方が良い
-            IDictionary<string, int> amountsPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountsPerStore) {
+            var amountsPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountsPerStore) {
                 Console.WriteLine($"{obj.Key} {obj.Value}");
             }
         }
--- Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/SalesCounter.cs
@@ -31,14 +31,14 @@ namespace SalesCalculator {
         //売上データを読み込み、Saleオブジェクトのリストを返す
         public static IEnumerable<Sale> ReadSales(string filePath) {
             //売上データを入れるリストオブジェクトを生成
-            List<Sale> sales = new List<Sale>();
+            var sales = new List<Sale>();
             //ファイルを一気に読み込み
-            string[] lines = File.ReadAllLines(filePath);
+            var lines = File.ReadAllLines(filePath);
             //読み込んだ行数分繰り返し
             foreach (string line in lines) {
-                string[] items = line.Split(',');
+                var items = line.Split(',');
                 //Saleオブジェクトを生成 
-                Sale sale = new Sale() {
+                var sale = new Sale() {
                     ShopName = items[0],
                     ProductCategory = items[1],
                     Amount = int.Parse(items[2])
