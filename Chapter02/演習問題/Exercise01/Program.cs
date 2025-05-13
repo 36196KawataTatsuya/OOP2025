72754bbbd2e0af202ba58e2121797d0c1a490a7f
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-13 10:03:56 +0900

    コメントの挿入とprintSongsメソッドの基盤作成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace Exercise01 {
-    internal class Program {
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise01 {
+    public class Program {
+
+        //2.1.3
         static void Main(string[] args) {
-            
+        
+
+
+
         }
+
+        //2.1.4
+        private static void printSongs(Song[] songs) {
+
+        }
+
+
     }
 }
