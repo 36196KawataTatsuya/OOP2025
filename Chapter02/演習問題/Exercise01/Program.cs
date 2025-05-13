efff3b1ba2448e1490f477558ef7d2f33b9f9760
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-13 11:16:14 +0900

    全体書式の整体

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -10,23 +10,25 @@ namespace Exercise01 {
                 new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
                 new Song("Close To You", "Carpenters", 276),
                 new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
+                new Song("I Will Always Love You", "Whitney Houston", 273)
             };
             printSongs(songs);
         }
 
         //2.1.4
         private static void printSongs(Song[] songs) {
-                Console.WriteLine("演目一覧\n");
+            Console.WriteLine("演目一覧\n");
             int timeTotal = 0;
+
             foreach (var song in songs) {
                 var playTime = TimeSpan.FromSeconds(song.Length);
                 Console.WriteLine($"タイトル / {song.Title}");
                 Console.WriteLine($"アーティスト / {song.ArtistName}");
                 Console.WriteLine($"演奏時間 / {playTime.Minutes}:{playTime.Seconds:00}");
-                Console.WriteLine("\n");
+                Console.WriteLine("---------------------");
                 timeTotal = timeTotal + song.Length;
             }
+
             var totalTime = TimeSpan.FromSeconds(timeTotal);
             Console.WriteLine($"全体演奏時間 / {totalTime.Minutes}：{totalTime.Seconds:00}");
 
@@ -34,4 +36,3 @@ namespace Exercise01 {
 
     }
 }
-    
