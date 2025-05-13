52e3acc73256854a06000db87667e075bd50cf72
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-13 10:30:57 +0900

    2.1.1~2.1.4の完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -5,17 +5,29 @@ namespace Exercise01 {
 
         //2.1.3
         static void Main(string[] args) {
-        
-
-
-
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
+            printSongs(songs);
         }
 
         //2.1.4
         private static void printSongs(Song[] songs) {
+            foreach (var song in songs) {
+                int min = song.Length / 60;
+                int sec = song.Length % 60;
+                Console.WriteLine($"タイトル / {song.Title}");
+                Console.WriteLine($"アーティスト / {song.ArtistName}");
+                Console.WriteLine($"演奏時間 / {min}:{sec:00}");
+                Console.WriteLine("\n");
+            }
 
         }
 
-
     }
 }
+    
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -7,11 +7,15 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     //2.1.1
     public class Song {
+        public string Title { get; set; } = string.Empty;
+        public string ArtistName { get; set; } = string.Empty;
+        public int Length { get; set; }
 
+        //2.1.2
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
     }
-
-    //2.1.2
-
-
-
 }
