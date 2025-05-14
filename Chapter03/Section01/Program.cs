4b0d910cfa35c0acc6ff49f6c32101ee30281052
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-14 12:10:04 +0900

    デリゲートによる実現

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,31 +1,31 @@
 ﻿namespace Section01 {
     internal class Program {
+
+        public delegate bool Judgement(int value); //デリゲートの宣言
+
         static void Main(string[] args) {
-            Console.WriteLine(Count(ParseNum())); //入力した数値を配列から検索して個数を出力
+            Judgement judge = IsOdd;
+            Console.WriteLine(Count(InputArray(), judge)); //入力した数値を配列から検索して個数を出力
 
         }
 
-        static int ParseNum() {
-            int countNum = 0;
-            while (true) {
-                Console.Write("カウントしたい数値：");
-                string? input = Console.ReadLine();
-
-                if (int.TryParse(input, out countNum)) {
-                    break; // 正常に入力されたらループ終了    
-                } else { // 変換に失敗したら再入力
-                    Console.WriteLine("無効な入力です。数字を入力してください。\n");
-                    continue;
-                }
-            }
-            return countNum;
-        }
-
-        static int Count(int num) {
+        static int[] InputArray() {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            return numbers;
+        }
+
+        static bool IsEven(int n) { //偶数を検索する条件を設定
+            return n % 2 == 0;
+        }
+
+        static bool IsOdd(int n) { //奇数を検索する条件を設定
+            return n % 2 == 1;
+        }
+
+        static int Count(int[] numbers, Judgement judge) {
             var count = 0;
             foreach (var n in numbers) {
-                if (n == num) {
+                if (judge(n) == true) {
                     count++;
                 }
             }
