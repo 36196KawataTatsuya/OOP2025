ca653309017601850790ac14f9b3eac53e7c70ed
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-14 10:52:45 +0900

    0514演習 完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -5,42 +5,88 @@ namespace Exercise01 {
 
         //2.1.3
         static void Main(string[] args) {
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273)
-            };
+            Console.WriteLine("***** 曲の登録 *****");
+            List<Song> songs = writeSongs();
             printSongs(songs);
         }
 
-        //2.1.4
-        private static void printSongs(Song[] songs) {
-            Console.WriteLine("演目一覧\n");
-            int timeTotal = 0;
+        private static List<Song> writeSongs() {
+            var songs = new List<Song>();
+            int length = 0;
 
-            foreach (var song in songs) {
-                var playTime = TimeSpan.FromSeconds(song.Length);
-                Console.WriteLine($"タイトル / {song.Title}");
-                Console.WriteLine($"アーティスト / {song.ArtistName}");
-                Console.WriteLine($"演奏時間 / {playTime.Minutes}:{playTime.Seconds:00}");
-                Console.WriteLine("---------------------");
-                timeTotal = timeTotal + song.Length;
+            while (true) {
+                Console.Write("曲名：");
+                var Title = Console.ReadLine();
+                if (Title == "end") {
+                    break;
+                }
+                while (string.IsNullOrWhiteSpace(Title)) {
+                    Console.WriteLine("入力内容が不正です\n再度入力してください\n\n");
+                    Console.Write("曲名：");
+                    Title = Console.ReadLine();
+                }
+                if (Title == "end") {
+                    break;
+                }
+                Console.Write("アーティスト名：");
+                var ArtistName = Console.ReadLine();
+                while (string.IsNullOrWhiteSpace(ArtistName)) {
+                    Console.WriteLine("入力内容が不正です\n再度入力してください\n\n");
+                    Console.Write("アーティスト名：");
+                    ArtistName = Console.ReadLine();
+                }
+                while (true) {
+                    Console.Write("演奏時間(秒)：");
+                    string? lengthString = Console.ReadLine();
+                    while (string.IsNullOrWhiteSpace(lengthString)) {
+                        Console.WriteLine("入力内容が不正です\n再度入力してください\n\n");
+                        Console.Write("演奏時間(秒)：");
+                        lengthString = Console.ReadLine();
+                    }
+                    if (!int.TryParse(lengthString, out length)) {
+                        Console.WriteLine("数値として認識できませんでした\n再度入力してください\n");
+                        continue;
+                    }
+                    if (length < 0) {
+                        Console.WriteLine("負の数値を演奏時間にすることはできません\n再度入力してください\n");
+                        continue;
+                    }
+                    break;
+                }
+                songs.Add(new Song(Title, ArtistName, length));
             }
-
-            /*または以下でも可
-             *foreach (var song in songs) {
-             *  var timespanw = TimeSpan.FromSeconds(song.Length);
-             *  Console.WriteLine(@"{0},{1}{2:m\ss}",
-             *      song.Tittle, song.ArtistName, TimeSpan.FromSeconds(song.Length));
-             *  }
-             */
-            
-            var totalTime = TimeSpan.FromSeconds(timeTotal);
-            Console.WriteLine($"全体演奏時間 / {totalTime.Minutes}：{totalTime.Seconds:00}");
-
+            return songs;
         }
 
+
+
+
+        //2.1.4
+        private static void printSongs(List<Song> songs) {
+            if (songs.Count == 0) {
+                Console.WriteLine("データが1件も入力されていません\nそのままアプリケーションを終了します");
+            } else {
+                Console.WriteLine("---------------------");
+                Console.WriteLine("\n演目一覧\n");
+                int timeTotal = 0;
+                foreach (var song in songs) {
+                    var playTime = TimeSpan.FromSeconds(song.Length);
+                    Console.WriteLine($"タイトル / {song.Title}");
+                    Console.WriteLine($"アーティスト / {song.ArtistName}");
+                    Console.WriteLine($"演奏時間 / {playTime.Minutes}:{playTime.Seconds:00}");
+                    Console.WriteLine("---------------------");
+                    timeTotal = timeTotal + song.Length;
+                }
+                /*または以下でも可
+                 *foreach (var song in songs) {
+                 *  var timespanw = TimeSpan.FromSeconds(song.Length);
+                 *  Console.WriteLine(@"{0},{1}{2:m\ss}",
+                 *      song.Tittle, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+                 *  }
+                 */
+                var totalTime = TimeSpan.FromSeconds(timeTotal);
+                Console.WriteLine($"全体演奏時間 / {totalTime.Minutes}：{totalTime.Seconds:00}");
+            }
+        }
     }
 }
