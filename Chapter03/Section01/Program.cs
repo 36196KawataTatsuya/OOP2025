5de11c2cb2acf4afb41d9242f9218135e6654d5d
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-14 11:51:43 +0900

    コード3.1 微修正

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            Console.Write("カウントしたい数値：");
-            int countNum = int.Parse(Console.ReadLine());
-            Console.WriteLine(Count(countNum));
+            Console.WriteLine(Count(ParseNum())); //入力した数値を配列から検索して個数を出力
 
+        }
+
+        static int ParseNum() {
+            int countNum = 0;
+            while (true) {
+                Console.Write("カウントしたい数値：");
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out countNum)) {
+                    break; // 正常に入力されたらループ終了    
+                } else { // 変換に失敗したら再入力
+                    Console.WriteLine("無効な入力です。数字を入力してください。\n");
+                    continue;
+                }
+            }
+            return countNum;
         }
 
         static int Count(int num) {
