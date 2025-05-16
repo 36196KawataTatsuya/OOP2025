da0c52eedad16d53b76379f52ec3a52120c62ab5
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-16 15:46:27 +0900

    3.2.1 と 3.2.2 の完成

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,41 +1,61 @@
-﻿namespace Section01 {
+﻿
+using System.Xml.Linq;
+
+namespace Section01 {
     internal class Program {
 
         static void Main(string[] args) {
-            /*  Actionデリゲート
-             *      戻り値：なし
-             *      Action<T>
-             *          Tは引数の型
-             *      例：Action<int>
-             *  
-             *  Predicateデリゲート
-             *      戻り値：bool
-             *      Predicate<T>
-             *          Tは引数の型で、戻り値はbool型
-             *
-             *  Funcデリゲート
-             *      戻り値：TResultで指定した型
-             *      Func<T, TResult>
-             *          Tは引数の型
-             */
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
 
-            Console.WriteLine(Count(numbers, n => /*5,6,7,8,9*/ 5 <= n && n < 10));
-            //var numbers に入った数値が numbers, n =>... のnに入る
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
 
 
+            
         }
 
-        static int Count(int[] numbers, Predicate<int> judge) {
-            var count = 0;
-            foreach (var v in numbers) {
-                //引数で受け取ったメソッドを呼び出す
-                if (judge(v) == true) {
-                    count++;
+        private static void Exercise2_1(List<string> names) {
+            while (true) {
+                Console.WriteLine("都市名を入力 / 空行で終了");
+                var name = Console.ReadLine();
+                int index = names.FindIndex(s => s.Equals(name));
+                if (string.IsNullOrWhiteSpace(name)) {
+                    break;
+                } else if (index == -1) {
+                    Console.WriteLine("見つかりませんでした\nもう一度入力してください\n");
+                } else {
+                    Console.WriteLine($"インデックスが見つかりました：{index}");
                 }
             }
-            return count;
         }
-        
+
+        private static void Exercise2_2(List<string> names) {
+            Console.WriteLine(names.Count(n => n.ToString().Contains('o')));
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            
+        }
+
+        private static void Exercise2_4(List<string> names) {
+            
+        }
+
     }
 }
