9b0d4b55fcb08cf439ffd6eb94cdd4a0cad48046
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-16 13:20:51 +0900

    内容訂正

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,30 +1,26 @@
 ﻿namespace Section01 {
     internal class Program {
 
-        public delegate bool Judgement(int value); //デリゲートの宣言
+        public delegate bool Judgement(int value);  //デリゲートの宣言
 
         static void Main(string[] args) {
-            Judgement judge = IsOdd;
-            Console.WriteLine(Count(InputArray(), judge)); //入力した数値を配列から検索して個数を出力
 
-        }
-
-        static int[] InputArray() {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            return numbers;
+
+            Judgement judge = IsEven;
+            Console.WriteLine(Count(numbers, judge));
+
         }
 
-        static bool IsEven(int n) { //偶数を検索する条件を設定
+        //メソッドへ渡す処理
+        static bool IsEven(int n) {
             return n % 2 == 0;
-        }
-
-        static bool IsOdd(int n) { //奇数を検索する条件を設定
-            return n % 2 == 1;
         }
 
         static int Count(int[] numbers, Judgement judge) {
             var count = 0;
             foreach (var n in numbers) {
+                //引数で受け取ったメソッドを呼び出す
                 if (judge(n) == true) {
                     count++;
                 }
