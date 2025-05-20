845ca2c2b469d9b7ab1c81848bee11aab8757655
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-20 10:40:12 +0900

    Section04作成とOrderBy句の使用まで完成

---
+++ Chapter03/Section04/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Section04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong"
+            };
+            /*
+             * IEnumerable、もしくはListやDictionaryなどでLINQが使えるのでWhere句などを入れられる
+             * ただし、FineAllはListでしか使えない
+             */
+            IEnumerable<string> query = cities
+                .OrderBy(s => s[0]);
+
+            foreach (var s in query) {
+                Console.WriteLine(s);
+            }
+
+
+
+        }
+    }
+}
