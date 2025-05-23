acf50b4a7d4277c718ab410e689148a9e286f557
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-23 15:53:42 +0900

    合計・平均・偶数合計・奇数合計を作成

---
+++ LINQ/LinqSample/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LinqSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 10);
+
+            //合計値を出力
+            Console.WriteLine(numbers.Sum());
+            Console.WriteLine(numbers.Average());
+            Console.WriteLine("----------");
+            Console.WriteLine(numbers.Where(x => x % 2 == 0).Sum());
+            Console.WriteLine(numbers.Where(x => x % 2 == 1).Sum());
+
+        }
+    }
+}
