73be1bb173025a37375ed2304afc76f714751cb0
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-23 15:59:58 +0900

    .Where無し偶数Sum作成 / Max & Min作成

--- LINQ/LinqSample/Program.cs
+++ LINQ/LinqSample/Program.cs
@@ -7,9 +7,12 @@
             //合計値を出力
             Console.WriteLine(numbers.Sum());
             Console.WriteLine(numbers.Average());
+            Console.WriteLine(numbers.Max());
+            Console.WriteLine(numbers.Min());
             Console.WriteLine("----------");
             Console.WriteLine(numbers.Where(x => x % 2 == 0).Sum());
             Console.WriteLine(numbers.Where(x => x % 2 == 1).Sum());
+            Console.WriteLine(numbers.Sum(x => x % 2 == 0 ? x : 0)); 
 
         }
     }
