5c402b9e8356ab4e97468309526528e70fc2b904
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-23 15:32:37 +0900

    4.2.3 完成 + 4.2.1 & 2 の構文修正

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -21,10 +21,10 @@
 
             if (number < 0) {
                 Console.WriteLine(number);
-            } else if (0 <= number && number < 100) { //0から99は2倍
-                Console.WriteLine(number*2);
-            } else if (100 <= number && number < 500) { //100から499は3倍
-                Console.WriteLine(number*3);
+            } else if (/*0 <= number && */number < 100) { //0から99は2倍
+                Console.WriteLine(number * 2);
+            } else if (/*100 <= number && */number < 500) { //100から499は3倍
+                Console.WriteLine(number * 3);
             } else {
                 Console.WriteLine(number);
             }
@@ -45,10 +45,10 @@
                 case < 0:
                     Console.WriteLine(number);
                     break;
-                case >= 0 and < 100:
+                case < 100:
                     Console.WriteLine(number * 2);
                     break;
-                case >= 100 and < 500:
+                case < 500:
                     Console.WriteLine(number * 3);
                     break;
                 default:
@@ -57,10 +57,25 @@
             }
         }
 
-
-
         private static void Exercise3() {
+            int number;
+            while (true) {
+                var input = Console.ReadLine();
+                if (!int.TryParse(input, out number)) {
+                    Console.WriteLine("入力値に誤りがあります");
+                } else {
+                    break;
+                }
+            }
 
+            var result = number switch {
+                < 0 => number,
+                < 100 => number * 2,
+                < 500 => number * 3,
+                _ => number
+            };
+
+            Console.WriteLine(result);
         }
 
     }
