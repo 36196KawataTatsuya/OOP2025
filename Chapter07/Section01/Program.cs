5592be9f5a3d8dfd82c7da223b33c63f04ab68b0
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-27 12:10:45 +0900

    Section01 完成

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace Section01 {
+    public class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks();
+
+            //１．本の平均金額を表示
+            Console.WriteLine(books.Average(b => b.Price));
+
+            //２．本のページ合計を表示
+            Console.WriteLine(books.Sum(b => b.Pages));
+
+            //３．金額の安い書籍名と金額を表示
+            Console.WriteLine(books.OrderBy(b => b.Price).FirstOrDefault()?.Title + " / " + books.Min(b => b.Price));
+
+            //４．ページが多い書籍名とページ数を表示
+            Console.WriteLine(books.OrderByDescending(b => b.Price).FirstOrDefault()?.Title + " / " + books.Max(b => b.Price));
+
+            //５．タイトルに「物語」が含まれている書籍名をすべて表示
+            books.Where(b => b.Title.Contains("物語")).ToList().ForEach(f => Console.WriteLine(f.Title));
+
+        }
+    }
+}
