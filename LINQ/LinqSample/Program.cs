9b65b5e5648ca9208ec0861bf2fc720fd6abf4c5
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-27 10:12:38 +0900

    Chapter07_演習問題_Exercise01_7.1.1~7.1.5完成

--- LINQ/LinqSample/Program.cs
+++ LINQ/LinqSample/Program.cs
@@ -2,17 +2,15 @@
     internal class Program {
         static void Main(string[] args) {
 
-            var numbers = Enumerable.Range(1, 10);
+            var numbers = Enumerable.Range(1, 100);
 
             //合計値を出力
-            Console.WriteLine(numbers.Sum());
-            Console.WriteLine(numbers.Average());
-            Console.WriteLine(numbers.Max());
-            Console.WriteLine(numbers.Min());
-            Console.WriteLine("----------");
-            Console.WriteLine(numbers.Where(x => x % 2 == 0).Sum());
-            Console.WriteLine(numbers.Where(x => x % 2 == 1).Sum());
-            Console.WriteLine(numbers.Sum(x => x % 2 == 0 ? x : 0)); 
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
+
+        /*  foreach (var num in numbers) {
+        *       Console.WriteLine(num);
+        *   }
+        */  
 
         }
     }
