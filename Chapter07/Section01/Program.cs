d803482b4afe0c7786c8deb501dcda7e8fa42e9f
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-28 09:39:48 +0900

    3 - 同一金額の全書物を出力できる構文を追加

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,16 +7,23 @@ namespace Section01 {
             var books = Books.GetBooks();
 
             //１．本の平均金額を表示
-            Console.WriteLine(books.Average(b => b.Price));
+            Console.WriteLine(books.Average(b => b.Price).ToString("0.00"));
+            Console.WriteLine("----------------");
 
             //２．本のページ合計を表示
             Console.WriteLine(books.Sum(b => b.Pages));
+            Console.WriteLine("----------------");
 
             //３．金額の安い書籍名と金額を表示
             Console.WriteLine(books.OrderBy(b => b.Price).FirstOrDefault()?.Title + " / " + books.Min(b => b.Price));
+            Console.WriteLine("----------------");
+
+            books.Where(b => b.Price == books.Min(b => b.Price)).ToList().ForEach(f => Console.WriteLine(f.Title + "/" + f.Price));
+            Console.WriteLine("----------------");
 
             //４．ページが多い書籍名とページ数を表示
             Console.WriteLine(books.OrderByDescending(b => b.Price).FirstOrDefault()?.Title + " / " + books.Max(b => b.Price));
+            Console.WriteLine("----------------");
 
             //５．タイトルに「物語」が含まれている書籍名をすべて表示
             books.Where(b => b.Title.Contains("物語")).ToList().ForEach(f => Console.WriteLine(f.Title));
