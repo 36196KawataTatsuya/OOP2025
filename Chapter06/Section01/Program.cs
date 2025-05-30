ff0cc2d83b083802dd2aec6d11779c76314a1319
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-05-30 14:00:10 +0900

    演習問題6.1 完成

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = "APPLE";
+            var str2 = "apple";
+            var cultureInfo = new CultureInfo("ja-JP");
+            if (String.Compare(str1, str2, cultureInfo, CompareOptions.IgnoreKanaType) == 0) {
+                Console.WriteLine("同じ文字列です。");
+            } else {
+                Console.WriteLine("異なる文字列です。");
+            }
+
+        }
+    }
+}
---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("***** 文字列判定 *****");
+            Console.Write("最初の文字列を入力してください: ");
+            var str1 = RL();
+            Console.Write("次の文字列を入力してください: ");
+            var str2 = RL();
+            if (String.Compare(str1, str2, ignoreCase:true) == 0) {
+                Console.WriteLine("同じ文字列です。");
+            } else {
+                Console.WriteLine("異なる文字列です。");
+            }
+
+
+        }
+
+        public static string? RL() {
+            while (true) {
+                var str = Console.ReadLine() ?? string.Empty;
+                if (str == string.Empty) {
+                    Console.WriteLine("空の文字列は入力できません。もう一度入力してください。");
+                    Console.Write("文字列を入力してください: ");
+                    continue;
+                } else {
+                    return str;
+                }
+            }
+        }
+
+    }
+}
