230a6e7030a7ea00b5af48ad43805dcee90b002a
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-03 10:22:59 +0900

    6.4 完成

--- Chapter06/演習問題/Exercise04/Program.cs
+++ Chapter06/演習問題/Exercise04/Program.cs
@@ -2,9 +2,14 @@
     internal class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-
-
-
+            line.Split(';').ToList().ForEach(l => {
+                var keyValue = (l.Split('='));
+                if (keyValue.Length == 2) {
+                    Console.WriteLine($"{ToJapanese(keyValue[0])}：{keyValue[1]}");
+                } else {
+                    Console.WriteLine($"無効な形式: {l}");
+                }
+            });
 
         }
 
@@ -14,8 +19,13 @@
         /// <param name="key">"Novelist","BestWork","Born"</param>
         /// <returns>"「作家」,「代表作」,「誕生年」</returns>
         static string ToJapanese(string key) {
-
-            return ""; //エラーをなくすためのダミー
+            string label = key switch {
+                "Novelist" => "作家",
+                "BestWork" => "代表作",
+                "Born" => "誕生年",
+                _ => "引数keyは正しい値ではありません"
+            };
+            return label; //エラーをなくすためのダミー
         }
     }
 }
