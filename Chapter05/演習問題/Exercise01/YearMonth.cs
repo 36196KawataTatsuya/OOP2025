7acf977d412455c93f2ede5722dccda8b7d7a83f
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-06 14:50:59 +0900

    5.1.1 / 5.1.2 完成

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        //5.1.2
+        public bool Is21Century => Year >= 2001 && Year <= 2100;
+
+    }
+}
