0a3bcbf8909410b16b9f9f40fa224c4321e90286
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-10 11:23:43 +0900

    5.2.2 / 5.2.3 / 5.2.4 / 5.2.5 完成

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Exercise01;
+using System.Linq;
 
 namespace Exercise02 {
     public class Program {
@@ -23,18 +24,37 @@ namespace Exercise02 {
         }
 
         private static void Exercise2(YearMonth[] ymCollection) {
-
-        }
+            //ymCollection.ToList().ForEach(ym => {
+            //    Console.WriteLine($"{ym.Year}年{ym.Month:00}月");
+            //});
+            ymCollection.ToList().ForEach(ym => { Console.WriteLine(ym); });
+        }   
 
         //5.2.3
         //ここにメソッドを作成 (メソッド名 : FindFirst21C)
+        private static YearMonth? FindFirst21C(YearMonth[] ymCollection) {
+            //最初に見つかった21世紀のYearMonthオブジェクトを返すメソッド
+            foreach (var ym in ymCollection) {
+                if (ym.Is21Century) {
+                    return ym;
+                }
+            }
+            // return ymCollection.FirstOrDefault(ym => ym.Is21Century);
+            return null;
+        }
 
         private static void Exercise4(YearMonth[] ymCollection) {
-
+            var result = FindFirst21C(ymCollection);
+            if (result == null) {
+                Console.WriteLine("21世紀のデータはありません");
+            } else {
+                Console.WriteLine($"{result.Year}");
+            }
         }
 
         private static void Exercise5(YearMonth[] ymCollection) {
-
+            YearMonth[] nextMonthCollection = ymCollection.Select(yc => yc.AddOneMonth()).ToArray();
+            Array.ForEach(nextMonthCollection, nMC => Console.WriteLine(nMC));
         }
 
     }
