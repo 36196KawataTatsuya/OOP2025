cc194760e64b3b7a67725b897eec4de6b9bbdc8d
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-11 10:17:33 +0900

    8.1.1コメント化 / 8.1.2完成

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -11,7 +11,7 @@
         }
 
         private static void Exercise1(string text) {
-            var words = new Dictionary<char, int>();
+            /*var words = new Dictionary<char, int>();
             foreach (var c in text.ToUpper()) {
                 if (char.IsLetter(c) && 'A' <= c && c <= 'Z') {
                     if (!words.ContainsKey(c)) {
@@ -27,6 +27,22 @@
                 }
             }
             //words.ToList().ForEach(w => Console.WriteLine($"{w.Key}: {w.Value}"));
+            */
+            var words = new SortedDictionary<char, int>();
+            foreach (var c in text.ToUpper()) {
+                if (char.IsLetter(c) && 'A' <= c && c <= 'Z') {
+                    if (!words.ContainsKey(c)) {
+                        words[c] = 1;
+                    } else {
+                        words[c]++;
+                    }
+                }
+            }
+            foreach (var writeLine in words) {
+                if (writeLine.Value != 0) {
+                    Console.WriteLine($"{writeLine.Key}: {writeLine.Value}");
+                }
+            }
         }
 
         private static void Exercise2(string text) {
