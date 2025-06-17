196ae4e51b5f77aa55404b2d556d835eb22d78ce
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-17 10:33:25 +0900

    8章演習問題_完成

--- Chapter08/Exercise02/Program.cs
+++ Chapter08/Exercise02/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Exercise02 {
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Exercise02 {
     public class Program {
         static void Main(string[] args) {
             // コンストラクタの呼び出し
@@ -7,6 +9,25 @@
             // Addメソッドの呼び出し例
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
+
+            //8.2.3 (Countの呼び出し例)
+            var count = abbrs.Count;
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            //8.2.3 (Removeの呼び出し例)
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine(abbrs.Count);
+            }
+
+            //すでに削除してあるので、falseが返る
+            if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            //8.2.4
+            var query = abbrs.GetAll().Where(x => x.Key.Length == 3);
 
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
