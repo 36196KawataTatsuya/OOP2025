c694518ff202c30ead9f2797397fdb56a4ff9a80
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-24 13:39:04 +0900

    Section01 曜日出力完成

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var today = new DateTime(2025,7,12);
+            var now = DateTime.Now;
+
+            Console.WriteLine($"Today:{today.Month}");
+            Console.WriteLine($"Now:{now}");
+
+            //自分の生年月日は何曜日かをプログラムから調べる
+            var birthday = new DateTime(2006, 2, 20);
+            var dayOfWeek = birthday.DayOfWeek;
+
+            Console.WriteLine($"誕生日の曜日は:{dayOfWeek}");
+
+            //うるう年の判定プログラムを作成する
+
+
+
+        }
+    }
+}
