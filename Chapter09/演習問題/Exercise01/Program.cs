a2a696ddf98018eeba19b347890224f67ff160c6
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-06-27 09:56:12 +0900

    Chapter09 / 01の1完成

---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            // 2024/03/09 19:03
+            // string.Formatを使った例
+            var formattedDate = string.Format("{0:yyyy/MM/dd HH:mm}",dateTime);
+            Console.WriteLine(formattedDate);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            // 2024年03月09日 19時03分09秒
+            // DateTime.ToStringを使った例
+
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+        }
+
+
+
+
+
+
+
+    }
+}
+
