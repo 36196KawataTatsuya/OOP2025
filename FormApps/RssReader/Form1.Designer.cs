6a46a682d422f4c3949a4c5ba410744b609008c0
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-07-18 14:25:18 +0900

    オブジェクトの名称変更とwebviewの戻る・進む・再読み込み

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -23,58 +23,198 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            tbUrl = new TextBox();
             btRssGet = new Button();
             lbTitles = new ListBox();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            urlSelect = new Button();
+            urlClear = new Button();
+            rollback = new Button();
+            forward = new Button();
+            reflesh = new Button();
+            favoriteAdd = new Button();
+            favoriteLabel = new Label();
+            favoriteRemove = new Button();
+            urlComboBox = new ComboBox();
+            favoriteName = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
             SuspendLayout();
-            // 
-            // tbUrl
-            // 
-            tbUrl.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            tbUrl.Location = new Point(12, 12);
-            tbUrl.Name = "tbUrl";
-            tbUrl.Size = new Size(493, 29);
-            tbUrl.TabIndex = 0;
             // 
             // btRssGet
             // 
             btRssGet.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            btRssGet.Location = new Point(511, 12);
+            btRssGet.Location = new Point(620, 12);
             btRssGet.Name = "btRssGet";
-            btRssGet.Size = new Size(104, 29);
+            btRssGet.Size = new Size(106, 29);
             btRssGet.TabIndex = 1;
-            btRssGet.Text = "取得";
+            btRssGet.Text = "データ取得";
             btRssGet.UseVisualStyleBackColor = true;
             btRssGet.Click += btRssGet_Click;
             // 
             // lbTitles
             // 
-            lbTitles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             lbTitles.FormattingEnabled = true;
             lbTitles.ItemHeight = 15;
             lbTitles.Location = new Point(12, 47);
             lbTitles.Name = "lbTitles";
-            lbTitles.Size = new Size(600, 424);
+            lbTitles.Size = new Size(714, 124);
             lbTitles.TabIndex = 2;
+            lbTitles.Click += lbTitles_Click;
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(12, 211);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(933, 442);
+            webView21.TabIndex = 3;
+            webView21.ZoomFactor = 1D;
+            // 
+            // urlSelect
+            // 
+            urlSelect.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            urlSelect.Location = new Point(12, 11);
+            urlSelect.Name = "urlSelect";
+            urlSelect.Size = new Size(97, 29);
+            urlSelect.TabIndex = 1;
+            urlSelect.Text = "選択...";
+            urlSelect.UseVisualStyleBackColor = true;
+            urlSelect.Click += urlSelect_Click;
+            // 
+            // urlClear
+            // 
+            urlClear.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            urlClear.Location = new Point(115, 11);
+            urlClear.Name = "urlClear";
+            urlClear.Size = new Size(97, 29);
+            urlClear.TabIndex = 1;
+            urlClear.Text = "クリア";
+            urlClear.UseVisualStyleBackColor = true;
+            urlClear.Click += urlClear_Click;
+            // 
+            // rollback
+            // 
+            rollback.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            rollback.Location = new Point(12, 176);
+            rollback.Name = "rollback";
+            rollback.Size = new Size(97, 29);
+            rollback.TabIndex = 1;
+            rollback.Text = "戻る";
+            rollback.UseVisualStyleBackColor = true;
+            rollback.Click += rollback_Click;
+            // 
+            // forward
+            // 
+            forward.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            forward.Location = new Point(115, 177);
+            forward.Name = "forward";
+            forward.Size = new Size(97, 29);
+            forward.TabIndex = 1;
+            forward.Text = "進む";
+            forward.UseVisualStyleBackColor = true;
+            forward.Click += forward_Click;
+            // 
+            // reflesh
+            // 
+            reflesh.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            reflesh.Location = new Point(218, 176);
+            reflesh.Name = "reflesh";
+            reflesh.Size = new Size(97, 29);
+            reflesh.TabIndex = 1;
+            reflesh.Text = "再読み込み";
+            reflesh.UseVisualStyleBackColor = true;
+            reflesh.Click += reflesh_Click;
+            // 
+            // favoriteAdd
+            // 
+            favoriteAdd.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            favoriteAdd.Location = new Point(683, 177);
+            favoriteAdd.Name = "favoriteAdd";
+            favoriteAdd.Size = new Size(97, 29);
+            favoriteAdd.TabIndex = 1;
+            favoriteAdd.Text = "登録";
+            favoriteAdd.UseVisualStyleBackColor = true;
+            favoriteAdd.Click += favoriteAdd_Click;
+            // 
+            // favoriteLabel
+            // 
+            favoriteLabel.AutoSize = true;
+            favoriteLabel.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            favoriteLabel.Location = new Point(321, 181);
+            favoriteLabel.Name = "favoriteLabel";
+            favoriteLabel.Size = new Size(118, 20);
+            favoriteLabel.TabIndex = 5;
+            favoriteLabel.Text = "お気に入り名称：";
+            // 
+            // favoriteRemove
+            // 
+            favoriteRemove.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            favoriteRemove.Location = new Point(786, 177);
+            favoriteRemove.Name = "favoriteRemove";
+            favoriteRemove.Size = new Size(97, 29);
+            favoriteRemove.TabIndex = 1;
+            favoriteRemove.Text = "登録解除";
+            favoriteRemove.UseVisualStyleBackColor = true;
+            favoriteRemove.Click += favoriteRemove_Click;
+            // 
+            // urlComboBox
+            // 
+            urlComboBox.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            urlComboBox.FormattingEnabled = true;
+            urlComboBox.Location = new Point(218, 11);
+            urlComboBox.Name = "urlComboBox";
+            urlComboBox.Size = new Size(396, 29);
+            urlComboBox.TabIndex = 6;
+            // 
+            // favoriteName
+            // 
+            favoriteName.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            favoriteName.FormattingEnabled = true;
+            favoriteName.Location = new Point(445, 177);
+            favoriteName.Name = "favoriteName";
+            favoriteName.Size = new Size(232, 29);
+            favoriteName.TabIndex = 6;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(624, 476);
+            ClientSize = new Size(957, 665);
+            Controls.Add(favoriteName);
+            Controls.Add(urlComboBox);
+            Controls.Add(favoriteLabel);
+            Controls.Add(webView21);
             Controls.Add(lbTitles);
+            Controls.Add(urlClear);
+            Controls.Add(favoriteRemove);
+            Controls.Add(favoriteAdd);
+            Controls.Add(reflesh);
+            Controls.Add(forward);
+            Controls.Add(rollback);
+            Controls.Add(urlSelect);
             Controls.Add(btRssGet);
-            Controls.Add(tbUrl);
             Name = "Form1";
             Text = "RSSリーダー";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private TextBox tbUrl;
         private Button btRssGet;
         private ListBox lbTitles;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private Button urlSelect;
+        private Button urlClear;
+        private Button rollback;
+        private Button forward;
+        private Button reflesh;
+        private Button favoriteAdd;
+        private Label favoriteLabel;
+        private Button favoriteRemove;
+        private ComboBox urlComboBox;
+        private ComboBox favoriteName;
     }
 }
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Security.Policy;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
@@ -11,31 +12,68 @@ namespace RssReader {
             InitializeComponent();
         }
 
-        private void btRssGet_Click(object sender, EventArgs e) {
-            if (tbUrl.Text == "") {
+        private async void btRssGet_Click(object sender, EventArgs e) {
+            if (string.IsNullOrWhiteSpace(urlComboBox.Text)) {
                 MessageBox.Show("URLが正しく入力されていません\nテキストボックスを確認してください", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
-            } else if (!Uri.TryCreate(tbUrl.Text, UriKind.Absolute, out Uri? result)) {
+            } else if (!Uri.TryCreate(urlComboBox.Text, UriKind.Absolute, out Uri? result)) {
                 MessageBox.Show("入力された文字列は有効なURLではありません\n正しいURLの形式で入力してください", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            using (var wc = new WebClient()) {
-                var url = wc.OpenRead(tbUrl.Text);
-                XDocument xdoc = XDocument.Load(url); //RSSの取得
+            using (var hc = new HttpClient()) {
+
+                string xml = await hc.GetStringAsync(urlComboBox.Text);
+                XDocument xdoc = XDocument.Parse(xml); //RSSの取得
 
                 items = xdoc.Root!.Descendants("item")
                     .Select(x => new ItemData {
-                        Title = (string?)x.Element("title") ?? (string?)x.Element("Title") ?? "No Title"
+                        Title = (string?)x.Element("title") ?? "No Title",
+                        Link = (string?)x.Element("link") ?? "No Link"
                     }).ToList();
             }
             lbTitles.Items.Clear();
-            //foreach (var item in items) {
-            //    lbTitles.Items.Add(item.Title);
-            //}
-            items.ForEach(x => lbTitles.Items.Add(x.Title));
-            
+            lbTitles.Items.AddRange(items.ToArray());
+            lbTitles.DisplayMember = "Title";
+
         }
 
 
+        private void lbTitles_Click(object sender, EventArgs e) {
+            string? linkUrl = null;
+            if (lbTitles.SelectedItem is ItemData selectedItem) {
+                linkUrl = selectedItem.Link;
+            }
+            if (!string.IsNullOrWhiteSpace(linkUrl) && Uri.TryCreate(linkUrl, UriKind.Absolute, out Uri? uriResult)) {
+                webView21.Source = uriResult;
+            }
+        }
+
+        private void rollback_Click(object sender, EventArgs e) {
+            webView21.GoBack();
+        }
+
+        private void forward_Click(object sender, EventArgs e) {
+            webView21.GoForward();
+        }
+
+        private void reflesh_Click(object sender, EventArgs e) {
+            webView21.Refresh();
+        }
+
+        private void favoriteAdd_Click(object sender, EventArgs e) {
+
+        }
+
+        private void favoriteRemove_Click(object sender, EventArgs e) {
+
+        }
+
+        private void urlClear_Click(object sender, EventArgs e) {
+            urlComboBox.Text = string.Empty;
+        }
+
+        private void urlSelect_Click(object sender, EventArgs e) {
+
+        }
     }
 }
