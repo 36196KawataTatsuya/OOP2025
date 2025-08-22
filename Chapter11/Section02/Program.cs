a8e4fdeaee4f46c41ebe597ecb0d5b0d478bdc15
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-08-22 11:18:33 +0900

    正規表現、最初のコード完成

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "private List<string> result = new List<string>();";
+            bool isMatch = Regex.IsMatch(text, @"\(\);$");
+
+            if (isMatch) {
+                Console.WriteLine(";で終わっています");
+            } else {
+                Console.WriteLine(";で終わっていません");
+            }
+        }
+    }
+}
