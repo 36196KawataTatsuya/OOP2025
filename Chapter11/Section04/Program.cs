3a6876e1dbfff972a079b9898b34f421fa3d12e8
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-09-02 13:23:51 +0900

    11章まで終了

--- Chapter11/Section04/Program.cs
+++ Chapter11/Section04/Program.cs
@@ -6,15 +6,13 @@ namespace Section04 {
 
             var lines = File.ReadAllLines("sample.txt");
             //問題11,4
-            var newLines = lines.Select(l => l.Replace("v4.0", "v5.0"));
+            var newLines = lines.Select(l => Regex.Replace(l, @"\bversion\s*=\s*""v4.0""", @"version=""v5.0""")).ToArray();
 
             File.WriteAllLines("sampleChange.txt", newLines);
 
             //確認用
             var text = File.ReadAllText("sampleChange.txt");
             Console.WriteLine(text);
-
-
 
         }
     }
