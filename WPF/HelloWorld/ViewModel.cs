9d461781cbdae42553224ea68ddef80c1944ab0a
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-10-02 13:39:30 +0900

    HelloWorldプロジェクトを作成

---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld {
+    class ViewModel {
+        public string GreetingMessage { get; set; } = "Hello World!";
+    }
+}
