f073e3b15efca1528fa5f49e0014e014bda0b3f5
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-10-02 14:59:21 +0900

    ボタンによる文字列の変更(MVVM)

--- WPF/HelloWorld/ViewModel.cs
+++ WPF/HelloWorld/ViewModel.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloWorld {
-    class ViewModel {
-        public string GreetingMessage { get; set; } = "Hello World!";
+
+    class ViewModel : BindableBase {
+        public ViewModel() {
+            ChangeMessageCommand = new DelegateCommand(
+                () => GreetingMessage = "See you next time");
+        }
+
+        private string _greetingMessage = "Hello World!";
+        public string GreetingMessage {
+            get => _greetingMessage;
+            set => SetProperty(ref _greetingMessage, value);
+        }
+
+        public DelegateCommand ChangeMessageCommand { get; }
+
     }
 }
