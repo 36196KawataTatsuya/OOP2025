665142931fd6d81beb11b386cf0561f48c71d3bc
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-10-30 14:59:58 +0900

    非同期完成

--- Chapter13/Section01/Program.cs
+++ Chapter13/Section01/Program.cs
@@ -1,14 +1,24 @@
 ﻿namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
-            var selected = Library.Books
-                .GroupBy(b => b.PublishedYear)
-                .Select(group => group.MaxBy(b => b.Price))
-                .OrderBy(b => b!.PublishedYear);
+            var groups = Library.Categories
+                .GroupJoin(Library.Books
+                , c => c.Id
+                , b => b.CategoryId,
+                (c, books) => new {
+                    Category = c.Name,
+                    Books = books,
+                });
 
-            foreach (var book in selected) {
-                Console.WriteLine($"{book!.PublishedYear}年 {book!.Title} ({book!.Price})");
+            //出力処理を入力して終わり
+            foreach (var group in groups) {
+                Console.WriteLine(group.Category);
+                foreach (var book in group.Books) {
+                    Console.WriteLine($"    {book.Title} ({book.PublishedYear}年)");
+                }
             }
+
+
         }
     }
 }
