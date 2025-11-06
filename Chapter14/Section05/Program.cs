ab44f798d9b211048cc3ed7fcebd3b50e8e5d709
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-11-06 15:17:31 +0900

    並列処理の比較サンプル

--- Chapter14/Section05/Program.cs
+++ Chapter14/Section05/Program.cs
@@ -4,15 +4,15 @@
 
             Console.WriteLine("並列処理有");
 
-            Parallel.For(0, 50, i => {
+            Parallel.For(0, 100, i => {
                 Console.WriteLine($"処理中：{i + 1}");
-                Thread.Sleep(1000);
+                Thread.Sleep(100);
                 Console.WriteLine($"処理完了：{i + 1}");
             });
 
             Console.WriteLine("並列処理無");
 
-            for (int i = 0; i < 50; i++) {
+            for (int i = 0; i < 100; i++) {
                 Console.WriteLine($"処理中：{i + 1}");
                 Thread.Sleep(100);
                 Console.WriteLine($"処理完了：{i + 1}");
