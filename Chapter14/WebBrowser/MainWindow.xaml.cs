305e8558ac8bbf3203195dce6b807b006fce8491
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-11-06 13:47:06 +0900

    戻る進むのみ実装

--- Chapter14/WebBrowser/MainWindow.xaml.cs
+++ Chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,16 +19,64 @@ namespace WebBrowser {
             InitializeComponent();
         }
 
+        private async void Window_Loaded(object sender, RoutedEventArgs e) {
+            await WebView.EnsureCoreWebView2Async(null);
+            if (WebView.CoreWebView2 != null) {
+                WebView.CoreWebView2.Navigate("https://www.google.com");
+            }
+        }
+        // 戻るボタン
         private void BackButton_Click(object sender, RoutedEventArgs e) {
-
+            if (WebView.CanGoBack) {
+                WebView.GoBack();
+            }
         }
 
-        private void FowardButton_Click(object sender, RoutedEventArgs e) {
-
+        // 進むボタン
+        private void ForwardButton_Click(object sender, RoutedEventArgs e) {
+            if (WebView.CanGoForward) {
+                WebView.GoForward();
+            }
         }
 
+        // 検索実行ボタン
         private void GoButton_Click(object sender, RoutedEventArgs e) {
 
         }
+
+        private void AddressBar_KeyDown(object sender, KeyEventArgs e) {
+
+        }
+
+        // URLナビゲート処理
+        private void NavigateToUrl(string url) {
+            // WebView2が初期化されていない場合は無効
+            if (WebView == null || WebView.CoreWebView2 == null) {
+                return;
+            }
+
+            // URLの形式を確認する
+            if (url.StartsWith("http://") || url.StartsWith("https://")) {
+                WebView.CoreWebView2.Navigate(url);
+            } else {    // デフォルトでhttps://を先頭に付ける
+                WebView.CoreWebView2.Navigate("https://" + url);
+            }
+
+            try {
+                // Uriオブジェクト作成を試行
+                Uri uri = new Uri(url);
+
+                // WebViewにナビゲート
+                WebView.Source = uri;
+            }
+            catch (UriFormatException) {
+                
+            }
+        }
+
+        private void WebView_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e) {
+
+        }
+
     }
 }
