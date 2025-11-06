3d551154bb13ffcd165622740484eed41bd61351
KawataTatsuya <oibc36196@ojs.ac.jp>
2025-11-06 15:16:51 +0900

    並列処理の比較

---
+++ Chapter14/Section05/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("並列処理有");
+
+            Parallel.For(0, 50, i => {
+                Console.WriteLine($"処理中：{i + 1}");
+                Thread.Sleep(1000);
+                Console.WriteLine($"処理完了：{i + 1}");
+            });
+
+            Console.WriteLine("並列処理無");
+
+            for (int i = 0; i < 50; i++) {
+                Console.WriteLine($"処理中：{i + 1}");
+                Thread.Sleep(100);
+                Console.WriteLine($"処理完了：{i + 1}");
+            }
+
+        }
+    }
+}
